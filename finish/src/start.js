48fb16bf4e4d85db0fe04740dcd0bf175cd6a465
Jason Barr <jason@jasonsbarr.com>
2023-03-30 12:26:02 -0500

    Main game menu

--- finish/src/start.js
+++ finish/src/start.js
@@ -1,5 +1,6 @@
 import figlet from "figlet";
 import { input } from "@jasonsbarr/simple-io";
+import { help } from "../help.js";
 
 export function start() {
   console.log(figlet.textSync("Welcome to Hangman!"));
@@ -7,5 +8,14 @@ export function start() {
 
   while (direction !== "Q") {
     direction = input("P for play, H for help, or Q for quit: ");
+
+    switch (direction) {
+      case "P":
+      case "H":
+        help();
+      case "Q":
+        console.log(figlet.textSync("Goodbye!"));
+        process.exit(0);
+    }
   }
 }
