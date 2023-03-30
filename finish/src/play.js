73d29924a858a0e136599f08f4a0f5ec3e4817d7
Jason Barr <jason@jasonsbarr.com>
2023-03-30 16:44:14 -0500

    Draw hangman function

--- finish/src/play.js
+++ finish/src/play.js
@@ -1,3 +1,53 @@
-import { input } from "@jasonsbarr/simple-io";
+import { input, readLines } from "@jasonsbarr/simple-io";
 
 export function play() {}
+
+/**
+ * Draws the hangman
+ * @param {number} used number of guesses used
+ */
+function drawMan(used) {
+  return used === 7 || used === 6 ? String.raw`       ____
+      |    |
+      |    O
+      |   \|/
+      |    |
+      |  _/ \_
+______|______` : used === 5 ? String.raw`       ____
+      |    |
+      |    O
+      |   \|/
+      |    |
+      |  _/
+______|______` : used === 4 ? String.raw`       ____
+      |    |
+      |    O
+      |   \|/
+      |    |
+      |
+______|______` : used === 3 ? String.raw`       ____
+      |    |
+      |    O
+      |   \|
+      |    |
+      |
+______|______` : used === 2 ? String.raw`       ____
+      |    |
+      |    O
+      |    |
+      |    |
+      |
+______|______` : used === 1 ? String.raw`       ____
+      |    |
+      |    O
+      |
+      |
+      |
+______|______` : String.raw`       ____
+      |    |
+      |
+      |
+      |
+      |
+______|______`;
+}
